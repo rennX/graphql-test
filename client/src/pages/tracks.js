ebf25c67ee9613bd1ea61acbf7ae4367a6a48be2
Aaron Skinner <aaron.skinner@aviture.us.com>
2021-01-30 12:23:28 -0600

    Add getTracks query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql } from '@apollo/client';
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        name
+        photo
+        id
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`
 
 /**
  * Tacks Page is Catstronauts home page.
