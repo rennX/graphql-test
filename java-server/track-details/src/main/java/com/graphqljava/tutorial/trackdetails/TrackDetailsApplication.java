0fa2f32cfc00319df5d772c8a53207399a7a6eae
Aaron Skinner <aaron.skinner@aviture.us.com>
2021-01-30 13:12:16 -0600

    Generated Spring Web project

---
+++ java-server/track-details/src/main/java/com/graphqljava/tutorial/trackdetails/TrackDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.trackdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackDetailsApplication.class, args);
+	}
+
+}
---
+++ java-server/track-details/src/test/java/com/graphqljava/tutorial/trackdetails/TrackDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqljava.tutorial.trackdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
